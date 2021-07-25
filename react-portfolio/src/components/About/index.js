9c3724ceb2f3fb69c2913887b7e71dbfdc865ce6
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-07-25 10:06:20 -0500

    editing the design

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -5,7 +5,7 @@ import coverImage from '../../assets/cover/cover.png';
 function About() {
   return (
     <section className="my-5" >
-    <h1 id="about">Who am I?</h1>
+    <h1 id="about">Kaan Emre Nazlioz </h1>
     <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     <div className="my-2">
      <p>
